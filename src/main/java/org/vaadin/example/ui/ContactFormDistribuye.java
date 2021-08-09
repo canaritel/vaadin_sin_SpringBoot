6af416bfd0009a7a1eb838b636cc1ae0ff300889
Antonio Gonzalez <toni@wificlick.es>
2021-08-10 00:22:26 +0100

    Proyecto en Java 11 y Vaadin 14 Creado Crud Usuarios Creado Crud Distribuidores Creando Crud Juegos Mejoras v.1.7

--- src/main/java/org/vaadin/example/ui/ContactFormDistribuye.java
+++ src/main/java/org/vaadin/example/ui/ContactFormDistribuye.java
@@ -6,6 +6,7 @@ import com.vaadin.flow.component.ComponentEventListener;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -13,6 +14,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.shared.Registration;
+import java.util.List;
 import org.vaadin.example.entities.Distribuye;
 
 public class ContactFormDistribuye extends FormLayout {
@@ -20,34 +22,58 @@ public class ContactFormDistribuye extends FormLayout {
     private final TextField textNombre = new TextField("Nombre");
     private final TextField textDireccion = new TextField("Dirección");
     private final TextField textCiudad = new TextField("Ciudad");
-    private final TextField textPais = new TextField("País");
+    //private final TextField textPais = new TextField("País");
+    private final ComboBox<String> comboBox = new ComboBox<>();
 
     private final Button save = new Button("Grabar");
     private final Button delete = new Button("Eliminar");
     private final Button close = new Button("Cancelar");
-    
+
     private final Binder<Distribuye> binder = new Binder<>(Distribuye.class);
 
     public ContactFormDistribuye() {
         addClassName("contact-form");
+        List<String> departmentList = CountryList.listadoPaises();
 
         VerticalLayout layout = new VerticalLayout(); //creo componente de línea vertical
 
-        binder.bind(textNombre, Distribuye::getIdDistribuidor, Distribuye::setIdDistribuidor);
-        binder.bind(textDireccion, Distribuye::getDireccion, Distribuye::setDireccion);
-        binder.bind(textCiudad, Distribuye::getCiudad, Distribuye::setCiudad);
-        binder.bind(textPais, Distribuye::getPais, Distribuye::setPais);
+        // establecemos valores para el comboBox
+        comboBox.setItems(departmentList);
+        comboBox.setLabel("País");
+        comboBox.setPlaceholder("Elija país o busque introduciendo el nombre del país");
 
-        textNombre.setEnabled(false);
+        //binder.bind(textNombre, Distribuye::getIdDistribuidor, Distribuye::setIdDistribuidor);
+        //binder.bind(textDireccion, Distribuye::getDireccion, Distribuye::setDireccion);
+        //binder.bind(textCiudad, Distribuye::getCiudad, Distribuye::setCiudad);
+        //binder.bind(comboBox, Distribuye::getPais, Distribuye::setPais);
+        binder.forField(textNombre)
+                .withValidator(name -> name.length() >= 5, "El nombre debe tener al menos 5 carácteres")
+                .bind(Distribuye::getIdDistribuidor, Distribuye::setIdDistribuidor);
+
+        binder.forField(textDireccion)
+                .withValidator(name -> name.length() >= 5, "La dirección debe tener al menos 5 carácteres")
+                .bind(Distribuye::getDireccion, Distribuye::setDireccion);
+
+        binder.forField(textCiudad)
+                .withValidator(name -> name.length() >= 3, "La ciudad debe tener al menos 3 carácteres")
+                .bind(Distribuye::getCiudad, Distribuye::setCiudad);
+
+        binder.forField(comboBox)
+                .withValidator(name -> name.length() >= 3, "El país debe tener al menos 3 carácteres")
+                .bind(Distribuye::getPais, Distribuye::setPais);
+        //binder.forField(comboBox)
+        //        .withConverter(String::valueOf, String::valueOf).bind(Distribuye::getPais, Distribuye::setPais);
+        //Por defecto el campo Nombre lo desactivamos
+        //textNombre.setEnabled(false);
+        //
         //añado los componentes a la vista
-        add(textNombre, textDireccion, textCiudad, textPais);
+        add(textNombre, textDireccion, textCiudad, comboBox);
         add(layout); //añado la línea verticaly así poner los botones debajo
         add(createButtonsLayout());
     }
 
     public void setContact(Distribuye distribuye) {
         binder.setBean(distribuye);
-
     }
 
     private Component createButtonsLayout() {
@@ -60,8 +86,8 @@ public class ContactFormDistribuye extends FormLayout {
         close.addClickShortcut(Key.ESCAPE);
 
         save.addClickListener(event -> validateAndSave());
-        delete.addClickListener(event -> fireEvent(new DeleteEvent(this, binder.getBean())));
-        close.addClickListener(event -> fireEvent(new CloseEvent(this)));
+        delete.addClickListener(event -> validateAndDelete());
+        close.addClickListener(event -> validateAndClose());
 
         binder.addStatusChangeListener(evt -> save.setEnabled(binder.isValid()));
 
@@ -76,7 +102,26 @@ public class ContactFormDistribuye extends FormLayout {
         }
     }
 
+    private void validateAndClose() {
+        //no es necesario validar los campos para Cerrar
+        //if (binder.isValid()) {
+        fireEvent(new CloseEvent(this));
+        // }
+    }
+
+    private void validateAndDelete() {
+        //no es necesario validar los campos para Eliminar
+        //if (binder.isValid()) {
+        fireEvent(new DeleteEvent(this, binder.getBean()));
+        // } 
+    }
+
+    public void EncenderCampo(boolean valor) {
+        textNombre.setEnabled(valor);
+    }
+
     // Eventos declarados en la misma clase
+    // Podremos de esta forma llamar a los métodos desde el método superior DistribuidorView
     public static abstract class ContactFormEvent extends ComponentEvent<ContactFormDistribuye> {
 
         private final Distribuye distribuye;
@@ -116,7 +161,4 @@ public class ContactFormDistribuye extends FormLayout {
         return getEventBus().addListener(eventType, listener);
     }
 
-    public void EncenderCampo (boolean valor){
-        textNombre.setEnabled(valor);
-    }
 }
