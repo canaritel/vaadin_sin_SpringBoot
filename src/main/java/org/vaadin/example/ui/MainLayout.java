936e97b04ddd62a0c3463efc01c3db31c49e9dee
Antonio Gonzalez <toni@wificlick.es>
2021-08-09 12:36:48 +0100

    Proyecto en Java 11 y Vaadin 14 Creado Crud Usuarios Creado Crud Distribuidores Creando Crud Juegos Mejoras v.1.5

--- src/main/java/org/vaadin/example/ui/MainLayout.java
+++ src/main/java/org/vaadin/example/ui/MainLayout.java
@@ -1,10 +1,9 @@
 package org.vaadin.example.ui;
 
-import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
 import com.vaadin.flow.component.dependency.CssImport;
-import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.H4;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
@@ -18,24 +17,10 @@ import com.vaadin.flow.router.RouterLink;
 @CssImport("./styles/shared-styles.css") //aplicamos CSS, en Netbeans ver en Files carpeta Frontend - Styles
 public class MainLayout extends AppLayout {
 
-    private Tabs menu;
-    private H2 viewTitle;
-
     public MainLayout() {
         createHeader();
         createDrawer(createTabs());
 
-        /*
-        // Use the drawer for the menu
-        setPrimarySection(Section.DRAWER);
-
-        // Make the nav bar a header
-        addToNavbar(true, createHeaderContent());
-
-        // Put the menu in the drawer
-        menu = createTabs();
-        addToDrawer(createDrawerContent(menu));
-         */
         // Ejemplo para aplicar en lectura de imágenes desde mi base datos ********************
         // byte[] imageBytes = getImageFromDB();
         // StreamResource resource = new StreamResource("fakeImageName.jpg", () -> new ByteArrayInputStream(imageBytes));
@@ -45,74 +30,34 @@ public class MainLayout extends AppLayout {
 
     private void createHeader() {
         //https://stackoverflow.com/questions/57553973/where-should-i-place-my-vaadin-10-static-files/57553974#57553974
-        Image img = new Image("images/logo.png", "Vaadin Televoip logo");
-        img.setHeight("48px");
+        // Cargamos las imágenes
+        Image imgLogo = new Image("images/logo.png", "Vaadin Televoip logo");
+        imgLogo.setHeight("48px");
 
-        Image img2 = new Image("images/avatar_60px.png", "avatar");
-        img2.setHeight("48px");
+        Image imgAvatar = new Image("images/avatar_60px.png", "avatar");
+        imgAvatar.setHeight("48px");
 
+        // Configuramos styling para el header
         HorizontalLayout header = new HorizontalLayout();
+        header.setId("header");
         header.addClassName("header");
-        header.add(new HorizontalLayout(new DrawerToggle(), img));
-        //header.setWidth("100%");
+        header.getThemeList().set("light", true);
         header.setWidthFull();
-        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        //header.setWidth("100%");
 
+        // configuración del alineamiento, barra de navegación e imagen
+        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.add(new HorizontalLayout(new DrawerToggle(), imgLogo));
+
+        // configuración del nuevo alineamiento para la imagen avatar 
         HorizontalLayout header2 = new HorizontalLayout();
-        header2.add(new HorizontalLayout(img2));
+        header2.add(new HorizontalLayout(imgAvatar));
         header2.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.END);
 
-        addToNavbar(header, header2);
-    }
+        H4 userName = new H4("Antonio González");
+        userName.setMaxWidth("80px"); //separación
 
-    private Component createHeaderContent() {
-        HorizontalLayout layout = new HorizontalLayout();
-
-        // Configure styling for the header
-        layout.setId("header");
-        layout.getThemeList().set("light", true);
-        layout.setWidthFull();
-        layout.setSpacing(false);
-        layout.setAlignItems(FlexComponent.Alignment.CENTER);
-
-        // Have the drawer toggle button on the left
-        layout.add(new DrawerToggle());
-
-        // Placeholder for the title of the current view.
-        // The title will be set after navigation.
-        viewTitle = new H2("Hola");
-        layout.add(viewTitle);
-
-        // A user icon
-        layout.add(new Image("images/avatar_60px.png", "Avatar"));
-
-        return layout;
-    }
-
-    private Component createDrawerContent(Tabs menu) {
-        Image img = new Image("images/logo.png", "Vaadin Televoip logo");
-        img.setHeight("48px");
-
-        VerticalLayout layout = new VerticalLayout();
-
-        // Configure styling for the drawer
-        layout.setSizeFull();
-        layout.setPadding(false);
-        layout.setSpacing(false);
-        layout.getThemeList().set("spacing-s", true);
-        layout.setAlignItems(FlexComponent.Alignment.STRETCH);
-
-        // Have a drawer header with an application logo
-        HorizontalLayout logoLayout = new HorizontalLayout();
-        logoLayout.setId("logo");
-        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
-
-        layout.add(img);
-
-        //logoLayout.add(new H1("My Project"));
-        // Display the logo and the menu in the drawer
-        layout.add(logoLayout, menu);
-        return layout;
+        addToNavbar(header, userName, header2);
     }
 
     private void createDrawer(Tabs tabs) {
@@ -120,6 +65,7 @@ public class MainLayout extends AppLayout {
         VerticalLayout layout = new VerticalLayout();
         layout.add("");
         addToDrawer(layout);
+
         //Muestra el menú Tabs
         addToDrawer(tabs);
     }
@@ -167,5 +113,56 @@ public class MainLayout extends AppLayout {
         //link1.setHighlightCondition(HighlightConditions.sameLocation()); //hago que se inicie en este enlace
         addToDrawer(new VerticalLayout(link1, link2, link3, link4));
     }
+    
+    private Component createHeaderContent() {
+        HorizontalLayout layout = new HorizontalLayout();
+
+        // Configure styling for the header
+        layout.setId("header");
+        layout.getThemeList().set("light", true);
+        layout.setWidthFull();
+        layout.setSpacing(false);
+        layout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        // Have the drawer toggle button on the left
+        layout.add(new DrawerToggle());
+
+        // Placeholder for the title of the current view.
+        // The title will be set after navigation.
+        viewTitle = new H2("Hola");
+        layout.add(viewTitle);
+
+        // A user icon
+        layout.add(new Image("images/avatar_60px.png", "Avatar"));
+
+        return layout;
+    }
+    
+     private Component createDrawerContent(Tabs menu) {
+        Image img = new Image("images/logo.png", "Vaadin Televoip logo");
+        img.setHeight("48px");
+
+        VerticalLayout layout = new VerticalLayout();
+
+        // Configure styling for the drawer
+        layout.setSizeFull();
+        layout.setPadding(false);
+        layout.setSpacing(false);
+        layout.getThemeList().set("spacing-s", true);
+        layout.setAlignItems(FlexComponent.Alignment.STRETCH);
+
+        // Have a drawer header with an application logo
+        HorizontalLayout logoLayout = new HorizontalLayout();
+        logoLayout.setId("logo");
+        logoLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+        layout.add(img);
+
+        //logoLayout.add(new H1("My Project"));
+        // Display the logo and the menu in the drawer
+        layout.add(logoLayout, menu);
+        return layout;
+    }
+    
      */
 }
