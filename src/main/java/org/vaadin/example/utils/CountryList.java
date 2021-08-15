ba4948dd891d9d45e2110fcf5d1bc24538a30598
Antonio Gonzalez <toni@wificlick.es>
2021-08-15 12:25:23 +0100

    Proyecto en Java 11 y Vaadin 14 Creado Crud Usuarios Creado Crud Distribuidores Creando Crud Juegos Mejoras v.1.9

---
+++ src/main/java/org/vaadin/example/utils/CountryList.java
@@ -0,0 +1,22 @@
+package org.vaadin.example.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class CountryList {
+
+    static private String[] locales;
+    static private final List<String> paises = new ArrayList<>();
+
+    public static List<String> listadoPaises() {
+        locales = Locale.getISOCountries();
+
+        for (String countryCode : locales) {
+            Locale obj = new Locale("", countryCode);
+            paises.add(obj.getDisplayCountry());
+        }
+        return paises;
+    }
+
+}
