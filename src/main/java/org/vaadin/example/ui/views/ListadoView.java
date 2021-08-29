01341a182b0a0bd989bad6e57ce74d2d6f981ab9
Antonio Gonzalez <toni@wificlick.es>
2021-08-29 22:05:55 +0100

    Proyecto en Java 11 y Vaadin 14 Creado Crud Usuarios Creado Crud Distribuidores Creando Crud Juegos Mejoras v.2.0.2

--- src/main/java/org/vaadin/example/ui/views/ListadoView.java
+++ src/main/java/org/vaadin/example/ui/views/ListadoView.java
@@ -1,38 +1,62 @@
 package org.vaadin.example.ui.views;
 
-import com.vaadin.flow.component.HasComponents;
-import com.vaadin.flow.component.HasStyle;
-import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.CssImport;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.FlexLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.select.Select;
-import com.vaadin.flow.component.template.Id;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import org.vaadin.example.entities.Listado;
+import java.util.ArrayList;
+import java.util.List;
+import org.vaadin.example.entities.Juego;
+import org.vaadin.example.services.JuegoService;
 import org.vaadin.example.ui.MainLayout;
+import org.vaadin.example.utils.ConvertToImage;
 
 @Route(value = "listado", layout = MainLayout.class)
 @PageTitle("Listado | Vaadin CRM")
 @CssImport("./styles/shared-styles.css")
-@Tag("listado-view")
-public class ListadoView extends VerticalLayout implements HasComponents, HasStyle {
+public class ListadoView extends FlexLayout {
 
-    @Id
-    private Select<String> sortBy;
-    private Listado listado =  new Listado();
+    private JuegoService juegoService;
+    private List<Juego> listado = new ArrayList<>();
 
     public ListadoView() {
-        //Le da al componente un nombre de clase CSS
+        if (juegoService == null) {
+            juegoService = new JuegoService();
+        }
 
-        //ortBy.setItems("Popularity", "Newest first", "Oldest first");
-        //sortBy.setValue("Popularity");
+        //Damos al componente un nombre de clase CSS
+        addClassName("horizontal-listado");  //nombre del componente CSS
 
-        //add(new LitPagination());
-        add(new Listado("Hola1",
-                        "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"));
+        updateList();
 
-        
+        for (int i = 0; i < listado.size(); i++) {
+            add(createListado(i));
+        }
+    }
+
+    private void updateList() {
+        listado = juegoService.listar("");
+    }
+
+    private VerticalLayout createListado(int index) {
+        VerticalLayout vertical = new VerticalLayout();
+        vertical.addClassName("horizontal-hijo");
+        vertical.setWidth("auto");
+
+        Juego juego = (Juego) listado.get(index);
+        Label label = new Label(juego.getTitulo());
+        Label label2 = new Label(juego.getSistemaOperativo());
+        Label label3 = new Label(juego.getUsuario().getNombre());
+        Label label4 = new Label(juego.getDistribuidor().getIdDistribuidor());
+        Label label5 = new Label(juego.getPrecio().toPlainString());
+        Image image = new Image(ConvertToImage.convertToStreamImage(juego.getImagen()), "");
+
+        vertical.add(label, image, label2, label3, label4, label5);
+
+        return vertical;
     }
 
 }
