0359720851854f2daca21fed995354d0bede0d4c
Antonio Gonzalez <toni@wificlick.es>
2021-09-01 12:55:41 +0100

    Proyecto en Java 11 y Vaadin 14 Creado Crud Usuarios Creado Crud Distribuidores Creando Crud Juegos Mejoras v.2.0.5

--- src/main/java/org/vaadin/example/ui/views/DistribuidorView.java
+++ src/main/java/org/vaadin/example/ui/views/DistribuidorView.java
@@ -3,6 +3,7 @@ package org.vaadin.example.ui.views;
 import com.vaadin.flow.component.Component;
 import org.vaadin.example.ui.forms.ContactFormDistribuye;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.html.Div;
@@ -26,17 +27,30 @@ import org.vaadin.example.ui.MainLayout;
 public class DistribuidorView extends VerticalLayout {
 
     private DistribuyeService distribuyeService;
-
+    //private ListDataProvider<Distribuye> dataProvider;
     private final Grid<Distribuye> grid = new Grid(Distribuye.class);  //creamos grid de tipo distribuye, similar a una tabla
     private final TextField filterText = new TextField();
     private ContactFormDistribuye form; //Crea un campo para el formulario para que pueda acceder a él desde otros métodos más adelante
+
+    private final Button button1 = new Button();
+    private final Button button2 = new Button();
+    private final Button button3 = new Button();
+    private final Button button4 = new Button();
+    private final Button button5 = new Button();
+    private final ComboBox<String> valueComboBox = new ComboBox<>();
+
+    private double totalPagina = 0;
+    private int totalPaginas = 0;
+    private int totalRegistros = 0;
+    private int itemsPagina = 15;
+    private int numeroPagina = 0;
 
     public DistribuidorView() {
         if (distribuyeService == null) {
             distribuyeService = new DistribuyeService();
         }
 
-        //Le da al componente un nombre de clase CSS
+        //Damos al componente un nombre de clase CSS
         addClassName("distribuye-view");  //nombre del componente CSS
         setSizeFull();
         configureGrid();
@@ -52,25 +66,29 @@ public class DistribuidorView extends VerticalLayout {
 
         //Crea un Div que envuelve el grid el formDis, le da un nombre de clase CSS y lo convierte en tamaño completo
         Div content = new Div(grid, form);
-        content.addClassName("content");
+        content.addClassName("content");  ////
         content.setSizeFull();
 
         //agrego todos los componentes al diseño principal
-        add(getToolBar(), content); //añado no solo componentes sino métodos como getToolBar
+        add(getToolBar(), content, configurePagination()); //añado no solo componentes sino métodos como getToolBar y configurePagination
+
         updateList();
         closeEditor();
     }
 
     private void configureGrid() {
+        //dataProvider = DataProvider.ofCollection(distribuyeService.listar(""));
+        grid.setWidthFull();
         grid.addClassName("contact-grid"); //añadimos la clase al grid
         grid.setSizeFull(); //ocupamos todo el espacio
         //mostramos las columnas con una función de mostrar listar
         grid.setItems(distribuyeService.listar("")); //sin orden al colocarse
+
+        datosPaginacion();
+
+        //grid.setItems(dataProvider.getItems());
+        //Borramos todas las columnas para añadirlas manualmente
         grid.removeAllColumns();
-        //grid.removeColumnByKey("idDistribuidor");
-        //grid.removeColumnByKey("direccion");
-        //grid.removeColumnByKey("ciudad");
-        //grid.removeColumnByKey("pais");
 
         //añadimos las columna y ponemos nombre a cada columna
         //grid.addColumn("idDistribuidor").setHeader("DISTRIBUIDOR");
@@ -79,14 +97,32 @@ public class DistribuidorView extends VerticalLayout {
                 .setSortable(true)
                 .setComparator(Distribuye::getIdDistribuidor);
 
-        grid.addColumn("direccion").setHeader("DIRECCIÓN");
-        grid.addColumn("ciudad").setHeader("CIUDAD");
-        grid.addColumn("pais").setHeader("PAÍS");
+        grid.addColumn(Distribuye::getDireccion).setHeader("DIRECCIÓN");
+        grid.addColumn(Distribuye::getCiudad).setHeader("CIUDAD");
+        grid.addColumn(Distribuye::getPais).setHeader("PAÍS");
 
         //ajusta la vista del grid para que los campos puedan leerse más apropiadamente (método general)
         grid.getColumns().forEach(col -> col.setAutoWidth(true));
+
+        // Sets the max number of items to be rendered on the grid for each page
+        //grid.setPageSize(15);  //todavía no se como implementarlo, no funciona 
         //activamos en grid tabla un evento que llama a editContact cuando se pulsa en algún registro
         grid.asSingleSelect().addValueChangeListener(evt -> editContact(evt.getValue()));
+
+    }
+
+    private void datosPaginacion() {
+        totalRegistros = distribuyeService.total(); //guardamos el total de registros
+        totalPagina = (double) totalRegistros / itemsPagina;
+        totalPaginas = (int) Math.ceil(totalPagina); //redondeamos hacia arriba y convertimos en int
+    }
+
+    private Component createDistribuidor(Distribuye distribuye) {
+        HorizontalLayout horizontal = new HorizontalLayout();
+        Icon icon = new Icon(VaadinIcon.STAR);
+        Label label = new Label(distribuye.getIdDistribuidor());
+        horizontal.add(icon, label);
+        return horizontal;
     }
 
     private void configureFilter() {
@@ -97,6 +133,101 @@ public class DistribuidorView extends VerticalLayout {
         filterText.addValueChangeListener(e -> updateList());
     }
 
+    private HorizontalLayout configurePagination() {
+        HorizontalLayout toolpagination = new HorizontalLayout();
+
+        button1.setIcon(VaadinIcon.BACKWARDS.create());
+        button2.setIcon(VaadinIcon.ANGLE_LEFT.create());
+        button3.setText("Página " + (numeroPagina + 1) + " de " + totalPaginas);
+        button4.setIcon(VaadinIcon.ANGLE_RIGHT.create());
+        button5.setIcon(VaadinIcon.FORWARD.create());
+
+        HorizontalLayout horizontalItems = new HorizontalLayout(configureItems());
+
+        HorizontalLayout horizontalCount = new HorizontalLayout();
+        Label label = new Label();
+        label.setText(totalRegistros + " registros");
+        label.setWidth("100px");
+        label.setHeight("35px");
+        horizontalCount.add(label);
+
+        HorizontalLayout horizontalPagination = new HorizontalLayout();
+        horizontalPagination.add(button1, button2, button3, button4, button5);
+
+        toolpagination.setWidthFull();
+        horizontalItems.setWidthFull();
+        horizontalPagination.setWidthFull();
+        horizontalCount.setWidthFull();
+
+        horizontalCount.setJustifyContentMode(JustifyContentMode.END);
+        toolpagination.setAlignSelf(Alignment.END, horizontalCount);
+        toolpagination.setAlignSelf(Alignment.CENTER, horizontalPagination);
+        toolpagination.setAlignSelf(Alignment.START, horizontalItems);
+
+        toolpagination.add(horizontalItems, horizontalPagination, horizontalCount);
+
+        createListener();
+
+        return toolpagination;
+    }
+
+    private HorizontalLayout configureItems() {
+        valueComboBox.setItems("5", "15", "30", "50");
+        valueComboBox.setValue("15");
+
+        HorizontalLayout headerItems = new HorizontalLayout();
+        headerItems.add(valueComboBox);
+
+        return headerItems;
+    }
+
+    private void createListener() {
+        valueComboBox.addValueChangeListener(e -> {
+            itemsPagina = Integer.valueOf(e.getSource().getValue());
+            datosPaginacion();
+            button3.setText("Página " + (numeroPagina + 1) + " de " + totalPaginas);
+            updateList();
+        });
+
+        button1.addClickListener(e -> {
+            if ((numeroPagina + 1) <= 1) {
+                return;
+            } else {
+                numeroPagina = 0;
+                grid.setItems(distribuyeService.listarPagination(filterText.getValue(), false, itemsPagina, numeroPagina));
+                button3.setText("Página " + (numeroPagina + 1) + " de " + totalPaginas);
+            }
+        });
+
+        button2.addClickListener(e -> {
+            if ((numeroPagina + 1) <= 1) {
+                return;
+            } else {
+                grid.setItems(distribuyeService.listarPagination(filterText.getValue(), false, itemsPagina, --numeroPagina));
+                button3.setText("Página " + (numeroPagina + 1) + " de " + totalPaginas);
+            }
+        });
+
+        button4.addClickListener(e -> {
+            if ((numeroPagina + 1) >= totalPaginas) {
+                return;
+            } else {
+                grid.setItems(distribuyeService.listarPagination(filterText.getValue(), false, itemsPagina, ++numeroPagina));
+                button3.setText("Página " + (numeroPagina + 1) + " de " + totalPaginas);
+            }
+        });
+
+        button5.addClickListener(e -> {
+            if ((numeroPagina + 1) >= totalPaginas) {
+                return;
+            } else {
+                numeroPagina = totalPaginas - 1;
+                grid.setItems(distribuyeService.listarPagination(filterText.getValue(), false, itemsPagina, numeroPagina));
+                button3.setText("Página " + (numeroPagina + 1) + " de " + totalPaginas);
+            }
+        });
+    }
+
     private HorizontalLayout getToolBar() {
         Button button = new Button("Añadir distribuidor", click -> addContact());
         add(button);
@@ -104,6 +235,10 @@ public class DistribuidorView extends VerticalLayout {
         HorizontalLayout toolbar = new HorizontalLayout(filterText, button);
         toolbar.addClassName("toolbar");
         return toolbar;
+    }
+
+    private void updateList() {
+        grid.setItems(distribuyeService.listarPagination(filterText.getValue(), false, itemsPagina, numeroPagina));
     }
 
     private void saveContact(ContactFormDistribuye.SaveEvent evt) {
@@ -146,18 +281,6 @@ public class DistribuidorView extends VerticalLayout {
         form.setVisible(false);
         removeClassName("editing");
         form.setContact(null);
-    }
-
-    private void updateList() {
-        grid.setItems(distribuyeService.listar(filterText.getValue()));
-    }
-
-    private Component createDistribuidor(Distribuye distribuye) {
-        HorizontalLayout horizontal = new HorizontalLayout();
-        Icon icon = new Icon(VaadinIcon.STAR);
-        Label label = new Label(distribuye.getIdDistribuidor());
-        horizontal.add(icon, label);
-        return horizontal;
     }
 
 }
